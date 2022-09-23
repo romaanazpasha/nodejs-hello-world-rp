53988c2524b4a88881d80417e06c20813e4fcee0
Romaana <b2521139@ben.edu>
2022-09-23 14:36:50 -0500

    version 4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/client'))
 const port = process.env.PORT || 3000
 app.get('/test', function(request, response) {
 	response.type('text/plain')
-	response.send('Node.js and Express running on port='+port)
+	response.send('rp Node.js and Express running on port='+port)
 })
 
 app.listen(port, function() {
